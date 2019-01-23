0fd31ff04ef43c599810cce2342e3af121233c40
Broderick Young <bxyoung89@gmail.com>
2019-01-23 00:30:38 -0600

    Lint fix.

--- src/pages/hop-directory/hop-detail/components/linkified-description.vue
+++ src/pages/hop-directory/hop-detail/components/linkified-description.vue
@@ -3,25 +3,21 @@
 </template>
 
 <script>
-	import StringService from '../../../../services/string-service';
-	import hops from "../../../../data/hop-directory";
+import StringService from "../../../../services/string-service";
+import hops from "../../../../data/hop-directory";
 
-	export default {
-		name: "linkified-description",
-		props:['text', 'hopId'],
-		computed: {
-			linkifiedText: function() {
-				return hops.reduce((currentText, hop, index) => {
-					if(this.hopId === index){
-						return currentText;
-					}
-					return StringService.replaceAll(currentText, hop.name, `<a href='#/hop-directory/${index}'>${hop.name}</a>`);
-				}, this.text);
-			},
-		}
-	}
+export default {
+	name: "linkified-description",
+	props: ["text", "hopId"],
+	computed: {
+		linkifiedText() {
+			return hops.reduce((currentText, hop, index) => {
+				if (this.hopId === index) {
+					return currentText;
+				}
+				return StringService.replaceAll(currentText, hop.name, `<a href='#/hop-directory/${index}'>${hop.name}</a>`);
+			}, this.text);
+		},
+	},
+};
 </script>
-
-<style scoped>
-
-</style>
