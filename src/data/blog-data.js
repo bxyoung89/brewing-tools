d3f8a5e42e014ad1174c25d76670c750cb0d87c6
Broderick Young <bxyoung89@gmail.com>
2019-03-20 18:11:16 -0500

    Lint fix.

--- src/data/blog-data.js
+++ src/data/blog-data.js
@@ -1,1 +1,5 @@
-export default [{"fileName":"2019-3-19.md","title":" Welcome to Brewing Tools!","formattedDate":"3/19/2019","timestamp":1555650000000},{"fileName":"2019-1-30.md","title":" Secret Chemistry IPA","formattedDate":"1/30/2019","timestamp":1551506400000}];
+export default [{
+	fileName: "2019-3-19.md", title: " Welcome to Brewing Tools!", formattedDate: "3/19/2019", timestamp: 1555650000000,
+}, {
+	fileName: "2019-1-30.md", title: " Secret Chemistry IPA", formattedDate: "1/30/2019", timestamp: 1551506400000,
+}];
