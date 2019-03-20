34de81dc8d0a8cefb8ba6af735c43aaaadb758e3
Broderick Young <bxyoung89@gmail.com>
2019-03-20 18:04:13 -0500

    Updated blog.

--- src/data/blog-data.js
+++ src/data/blog-data.js
@@ -1,5 +1,1 @@
-export default [{
-	fileName: "2019-1-30.md", title: " Secret Chemistry IPA", formattedDate: "1/30/2019", timestamp: 1551506400000,
-}, {
-	fileName: "2019-1-29.md", title: " Welcome to Brewing Tools!", formattedDate: "1/29/2019", timestamp: 1551420000000,
-}];
+export default [{"fileName":"2019-3-19.md","title":" Welcome to Brewing Tools!","formattedDate":"3/19/2019","timestamp":1555650000000},{"fileName":"2019-1-30.md","title":" Secret Chemistry IPA","formattedDate":"1/30/2019","timestamp":1551506400000}];
